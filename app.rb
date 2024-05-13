6d0166896b78302458b6f5c92c9e751a3b29f735
Mrazaza <164953388+Mrazaza@users.noreply.github.com>
2024-05-13 08:31:56 +0000

    final

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Dice Roll</h1>
   "
+  erb (:layout)
+end
+
+get '/process_url' do
+  "Hello World"
 end
